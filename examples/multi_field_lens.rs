ecf9682a45d682957867c854ee8a2ad0fc86308b
azarmadr <azarshaik454@gmail.com>
2022-04-30 10:23:16 +0530

    add an example with lens for multiple fields of a component

--- examples/multi_field_lens.rs
+++ examples/multi_field_lens.rs
@@ -25,7 +25,7 @@ pub struct BeTween<T> {
     start: Option<T>,
 }
 impl<T> BeTween<T> {
-    /// Construct a lens from a pair of getter functions
+    /// Construct a lens from lerping functions
     pub fn with_lerp<U>(lerp: U) -> Self
     where
         U: Fn(&mut T, &T, f32) + Send + Sync + 'static,
@@ -36,10 +36,7 @@ impl<T> BeTween<T> {
         }
     }
 }
-impl<T: Clone> Lens<T> for BeTween<T>
-where
-    T: Component,
-{
+impl<T: Clone> Lens<T> for BeTween<T> {
     fn lerp(&mut self, target: &mut T, ratio: f32) {
         if self.start.is_none() {
             self.start = Some(target.clone());
@@ -48,46 +45,6 @@ where
             (self.lerp)(target, start, ratio);
         }
     }
-}
-
-pub fn rot_seq(duration: std::time::Duration) -> Sequence<Transform> {
-    let start = 0.;
-    let end = std::f32::consts::PI / 2.;
-    let tween = |start, end| {
-        Tween::new(
-            EaseFunction::QuadraticIn,
-            TweeningType::Once,
-            duration,
-            TransformRotateYLens { start, end },
-        )
-    };
-    tween(start, end).then(tween(end, start))
-}
-pub fn vis_seq(duration: std::time::Duration, show: bool) -> Tween<Visibility> {
-    Tween::new(
-        EaseFunction::QuadraticIn,
-        TweeningType::Once,
-        2 * duration,
-        BeTween::with_lerp(move |c: &mut Visibility, _, r| c.is_visible = show ^ (r < 0.5)),
-    )
-}
-pub fn shake_seq(duration: std::time::Duration) -> Sequence<Transform> {
-    let tween = |s, e, i| {
-        Tween::new(
-            EaseFunction::ElasticInOut,
-            TweeningType::Once,
-            duration * i / 3,
-            BeTween::with_lerp(move |c: &mut Transform, _, r| {
-                c.rotation = Quat::from_rotation_z(s + (e - s) * r)
-            }),
-        )
-    };
-    let pi = std::f32::consts::PI;
-    Sequence::new((1..4).rev().map(|i| {
-        tween(0., pi / 12. / i as f32, i)
-            .then(tween(-pi / 12. / i as f32, -pi / 12. / i as f32, i))
-            .then(tween(-pi / 12. / i as f32, 0., i))
-    }))
 }
 
 fn setup(mut commands: Commands) {
@@ -139,9 +96,9 @@ fn setup(mut commands: Commands) {
             //BeTween::with_lerp(move |c: &mut(Visibility, Transform), _, r| {
             BeTween::with_lerp(move |c: &mut Transform, _, r| {
                 //c.0.is_visible = true ^ (r < 0.2);
-                c.translation = Vec3::new(x, screen_y, 50.).lerp(Vec3::new(x, -screen_y, 50.),r);
-                // rotation from s+delta*r
-                c.rotation = Quat::from_rotation_y(0. + 4.*std::f32::consts::PI * r);
+                c.translation = Vec3::new(x, screen_y, 50.).lerp(Vec3::new(x, -screen_y, 50.), r);
+                // rotation around y axis from 0 to 2pi
+                c.rotation = Quat::from_rotation_y(0. + 4. * std::f32::consts::PI * r);
             }),
         );
 
